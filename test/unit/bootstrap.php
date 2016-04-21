2c29dd2cbe5d4299ccbb920222af5b0591667420
Michael Rutz <michael.rutz@sclable.com>
2016-04-21 16:24:53 +0200

    add phpunit setup

---
+++ test/unit/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * This file is part of the Sclable Array Functions Package.
+ * For the full license information, please read the LICENSE
+ * file distributed with this source code.
+ */
+ 
+require dirname(dirname(__DIR__)) . '/vendor/autoload.php';
